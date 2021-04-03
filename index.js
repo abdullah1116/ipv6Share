c7ec3ba5fdf2e51780695c0a865749ddddd71a22
tmpabdullah <tmp.abdullah1@outlook.com>
2021-04-03 22:18:09 +0500

    add env

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const { serveVal, serveChangeVal } = require('./serveVal');
 const { getIps } = require('./getIps');
+const localtunnel = require('localtunnel');
+require('dotenv').config();
+process.env = Object.assign({ port: 1910 }, process.env);
 
 console.log('IPv6:', getIps());
 
-serveVal({ port: 1910 });
+serveVal({ port: process.env.port });
 serveChangeVal(getIps());
+
+(async () => {
+  const tunnel = await localtunnel({
+    port: process.env.port,
+    subdomain: 'ah1910',
+  });
+  console.log(tunnel.url);
+})();
